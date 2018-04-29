445b2e4028def1770143b89d36ccd64fc8250e1c
BenDHillier <ben@divdot.com>
2018-04-28 23:39:40 -0700

    add selectors to createSlice

--- src/createSlice/createSlice.js
+++ src/createSlice/createSlice.js
@@ -1,9 +1,6 @@
-import createTypes from './createTypes';
 import createStates from './createStates';
 import createReducer from './createReducer';
 import createDispatchers from './createDispatchers';
-import createActionCreators from './createActionCreators';
-import { fromJS } from 'immutable';
 
 export default function createSlice(slice, fields) {
   return {
--- src/createSlice/createSlice.test.js
+++ src/createSlice/createSlice.test.js
@@ -1,22 +1,26 @@
-// import chai from 'chai';
-// import createSlice from './createSlice';
-// import settable from '../properties/settable';
-// import { fromJS } from 'immutable';
-//
-// chai.should();
-//
+import chai from 'chai';
+import createSlice from './createSlice';
+import settable from '../properties/settable';
+import { fromJS } from 'immutable';
+
+chai.should();
+
 // describe('createSlice()', () => {
 //   const slice = 'slice';
 //   const key = 'exampleKey',
 //     initialState = 'initial',
 //     key1 = 'exampleKey1',
 //     initialState1 = 'initial1';
-// });
+//   const { reducer, selectors, dispatchers } = createSlice(slice, [
+//     { key: 'exampleKey', initialState: 0, properties: [settable] },
+//     { key: 'exampleKey1', initialState: false, properties: [toggleable] }
+//   ]);
 //
-// const { reducer, selectors, dispatchers } = createSlice(slice, [
-//   { key: 'exampleKey', initialState: 0, properties: [settable] },
-//   { key: 'exampleKey1', initialState: false, properties: [toggleable] }
-// ]);
+//   it('createSlice() gives correct initial state', () => {
+//     const state =
+//   });
+// });
+
 //
 // // dispatchers = {
 // //   setExampleKey: dispatch => value =>
